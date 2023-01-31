9cb4c4879102b42244b8210fd9076b7f1a65ca16
Dalton Tan <daltonyi@hotmail.com>
2023-01-31 15:16:31 +0800

    implement mod ctrl tap for curly bracket, add new combo

--- keyboards/lily58/keymaps/agentcosmic/keymap.c
+++ keyboards/lily58/keymaps/agentcosmic/keymap.c
@@ -28,21 +28,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  KC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_EQL,   KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
-   KC_LGUI, KC_LALT, MT(MOD_LCTL, KC_LBRC), LT(_FN, KC_SPC),   LT(_FN, KC_SPC), MT(MOD_RCTL, KC_RBRC), KC_RALT, A(KC_TAB)
+  KC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_EQL ,   KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
+            KC_LGUI, KC_LALT, LCTL_T(KC_LCBR), LT(_FN, KC_SPC),  LT(_FN, KC_SPC), RCTL_T(KC_RCBR), KC_RALT, A(KC_TAB)
 ),
 
 /* ENGRAM
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |      |  B   |  Y   |  O   |  U   |  ;   |                    |  '   |  L   |  D   |  W   |  V   |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------|      |      |RGB ON| HUE+ | SAT+ | VAL+ |
+ * |      |  C   |  I   |  E   |  A   |  ,   |-------.    ,-------|  .   |  H   |  T   |  S   |  N   |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
+ * |      |  G   |  X   |  J   |  K   |  Z   |-------|    |-------|  Q   |  R   |  M   |  F   |  P   |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |  FN  | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   |      |      |      | /       /       \      \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* COH
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -81,9 +81,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |  [   |   ]  |      |      |                    |      | Home |  Up  | PgU  |      |      |
+ * |      |      |      |      |      |      |                    |      | Home |  Up  | PgU  |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |  -   |  {   |   }  |   _  |      |-------.    ,-------| PtSc | Left | Down |Right |  '   |      |
+ * |      |  -   |  [   |   ]  |   _  |      |-------.    ,-------| PtSc | Left | Down |Right |  '   |      |
  * |------+------+------+------+------+------| BOOT  |    | BOOT  |------+------+------+------+------+------|
  * | L(2) | L(3) |      |      |      |      |-------|    |-------| INS  | End  |      | PgD  |  "   |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -94,8 +94,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_FN] = LAYOUT(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______, _______, KC_LBRC, KC_RBRC, _______, _______,                   _______, KC_HOME, KC_UP,   KC_PGUP, _______, _______,
-  TG(_NGM),KC_MINS, KC_LCBR, KC_RCBR, KC_UNDS, _______,                   KC_PSCR, KC_LEFT, KC_DOWN, KC_RGHT, KC_QUOT, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, KC_HOME, KC_UP,   KC_PGUP, _______, _______,
+  TG(_NGM),KC_MINS, KC_LBRC, KC_RBRC, KC_UNDS, _______,                   KC_PSCR, KC_LEFT, KC_DOWN, KC_RGHT, KC_QUOT, _______,
   TG(_COH),_______, _______, _______, _______, _______, QK_BOOT, QK_BOOT, KC_INS,  KC_END,  _______,  KC_PGDN, KC_DQUO, _______,
                             _______, _______, _______, _______,  _______, _______,  _______ , _______
 )
@@ -117,6 +117,7 @@ const uint16_t PROGMEM combo_ctrl_z[] = {KC_LSPO, KC_Z, COMBO_END};
 const uint16_t PROGMEM combo_ctrl_x[] = {KC_Z, KC_X, COMBO_END};
 const uint16_t PROGMEM combo_ctrl_c[] = {KC_X, KC_C, COMBO_END};
 const uint16_t PROGMEM combo_ctrl_v[] = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM combo_ctrl_b[] = {KC_V, KC_B, COMBO_END};
 const uint16_t PROGMEM combo_quote[] = {KC_L, KC_SCLN, COMBO_END};
 const uint16_t PROGMEM combo_dquote[] = {KC_SCLN, KC_ENT, COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {
@@ -130,6 +131,7 @@ combo_t key_combos[COMBO_COUNT] = {
   COMBO(combo_ctrl_x, LCTL(KC_X)),
   COMBO(combo_ctrl_c, LCTL(KC_C)),
   COMBO(combo_ctrl_v, LCTL(KC_V)),
+  COMBO(combo_ctrl_b, LCTL(KC_B)),
   COMBO(combo_quote, KC_QUOT),
   COMBO(combo_dquote, KC_DQUO),
 };
@@ -201,11 +203,29 @@ bool oled_task_user(void) {
 #endif // OLED_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // log keys to oled
   if (record->event.pressed) {
 #ifdef OLED_ENABLE
     set_keylog(keycode, record);
 #endif
     // set_timelog();
   }
+
+  // overcome limitation of using curly brackets with ctrl mod tap
+  switch (keycode) {
+      case LCTL_T(KC_LCBR):
+          if (record->tap.count && record->event.pressed) {
+              tap_code16(KC_LCBR); // Send KC_LCBR on tap
+              return false;        // Return false to ignore further processing of key
+          }
+          break;
+      case RCTL_T(KC_RCBR):
+          if (record->tap.count && record->event.pressed) {
+              tap_code16(KC_RCBR); // Send KC_RCBR on tap
+              return false;        // Return false to ignore further processing of key
+          }
+          break;
+  }
+
   return true;
 }
